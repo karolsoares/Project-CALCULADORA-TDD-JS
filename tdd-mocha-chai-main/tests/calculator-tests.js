356d6fd2616cee8b23c829ef7d12548084001c23
karolsoares <86377137+karolsoares@users.noreply.github.com>
2021-09-30 15:39:22 -0300

    Update calculator-tests.js

--- tdd-mocha-chai-main/tests/calculator-tests.js
+++ tdd-mocha-chai-main/tests/calculator-tests.js
@@ -11,5 +11,21 @@ describe('The Calculator', () => {
     it('Should sum two positive integers', () => {
       expect(calculator.sum(2, 3)).to.equal(5);
     });
-  
+
+    it('Deve somar um positivo e um negativo', () => {
+      expect(calculator.sum(-2, 3)).to.equal(1);
+    });
+
+    it('Multiplicar dois positivos', () => {
+      expect(calculator.vezes(10, 3)).to.equal(30);
+    });
+
+    it('Multiplicar dois negativos', () => {
+      expect(calculator.vezes(-9, -7)).to.equal(63);
+    });
+
+    it('Dividir dois positivos', () => {
+      expect(calculator.dividir(20, 2)).to.equal(10);
+    });
+
 });
